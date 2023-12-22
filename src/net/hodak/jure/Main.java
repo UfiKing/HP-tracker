ba1c87941347fd167745b877482efdd74bf7b521
ufi <jure@hodak.net>
2023-12-22 17:40:35 +0100

    Added more stuff

--- src/net/hodak/jure/Main.java
+++ src/net/hodak/jure/Main.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 
 public class Main {
     //Arrays to store the values of mobs
-    // [Name of mob] [Abreviation] [HP]
+    // [Name of mob] [Abbreviation] [HP]
+    static int nmb = 0;
     static int nmbOfMobs;
     static String[] array = new String[3];
     static String[] mob1 = new String[3];
@@ -23,28 +24,41 @@ public class Main {
         String input;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome");
-        System.out.println("Would you like to add a mob?");
-        input = scanner.nextLine();
-        if (input.toLowerCase(Locale.ROOT).equals("yes")){
-            System.out.println("You can add up to 10 mobs");
-            add();
-        }else{
-            System.out.println("Okay");
-            System.out.println("Shutting down");
-        }
-        for (int i = 0; i<3 ;i++){
-            System.out.println(array[i]);
+        while (true) {
+            System.out.println("What would you like to do?");
+            System.out.println("Add an enemy(Add), display current enemy's(mob), or go into encounter mode(enc)?");
+            input = scanner.nextLine();
+            if (input.toLowerCase(Locale.ROOT).equals("add")) {
+                add();
+            }else if (input.toLowerCase(Locale.ROOT).equals("mob")){
+                System.out.println("Displaying current mobs");
+                displayMobs();
+                System.out.println(" ");
+            }else if (input.toLowerCase(Locale.ROOT).equals("enc")){
+                encounter();
+            }else{
+                System.out.println("Sorry I didn't get that.");
+            }
+            /*for (int i = 0; i < 3; i++) {
+                System.out.println(array[i]);
+            }
+            */
 
+            //System.out.println(nmbOfMobs);
         }
-        System.out.println("Displaying current mobs");
-        displayMobs();
-        System.out.println(nmbOfMobs);
     }
 
     public static void add(){
         Scanner scanner = new Scanner(System.in);
-        int nmb = 0;
+
+        int nmbOfRemainingMobs = 0;
+        nmbOfRemainingMobs = 10-nmb;
         boolean bool = true;
+        if (nmb == 0){
+            System.out.println("You can add up to 10 mobs");
+        }else{
+            System.out.println("You can add " + nmbOfRemainingMobs + " enemy's");
+        }
         while (bool){
             System.out.println("Enter name of mob (ex. Zombie1)");
             String input = scanner.nextLine();
@@ -59,6 +73,7 @@ public class Main {
             array[2] = input;
             nmb++;
             nmbOfMobs++;
+
             switch (nmb) {
                 case 1 -> System.arraycopy(array, 0, mob1, 0, 3);
                 case 2 -> System.arraycopy(array, 0, mob2, 0, 3);
@@ -91,7 +106,6 @@ public class Main {
 
     }
 
-
     public static void displayMobs(){
         if (nmbOfMobs == 1){
             for (int i = 0; i<=2; i++){
@@ -101,6 +115,7 @@ public class Main {
             for (int i = 0; i<=2; i++){
                 System.out.print(mob1[i] + " ");
             }
+            System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob2[i] + " ");
             }
@@ -108,10 +123,10 @@ public class Main {
         }else if(nmbOfMobs==3){
             for (int i = 0; i<=2; i++){
                 System.out.print(mob1[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob2[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob3[i] + " ");
             }
@@ -120,12 +135,15 @@ public class Main {
             for (int i = 0; i<=2; i++){
                 System.out.print(mob1[i] + " ");
             }
+            System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob2[i] + " ");
             }
+            System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob3[i] + " ");
             }
+            System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob4[i] + " ");
             }
@@ -133,34 +151,38 @@ public class Main {
             for (int i = 0; i<=2; i++){
                 System.out.print(mob1[i] + " ");
             }
+            System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob2[i] + " ");
             }
+            System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob3[i] + " ");
             }
+            System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob4[i] + " ");
             }
+            System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob5[i] + " ");
             }
         }else if(nmbOfMobs==6){
             for (int i = 0; i<=2; i++){
                 System.out.print(mob1[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob2[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob3[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob4[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob5[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob6[i] + " ");
             }
@@ -189,25 +211,25 @@ public class Main {
         }else if(nmbOfMobs==8){
             for (int i = 0; i<=2; i++){
                 System.out.print(mob1[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob2[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob3[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob4[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob5[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob6[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob7[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob8[i] + " ");
             }
@@ -242,31 +264,31 @@ public class Main {
         }else if(nmbOfMobs==10){
             for (int i = 0; i<=2; i++){
                 System.out.print(mob1[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob2[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob3[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob4[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob5[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob6[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob7[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob8[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob9[i] + " ");
-            }
+            }System.out.println(" ");
             for (int i = 0; i<=2; i++){
                 System.out.print(mob10[i] + " ");
             }
@@ -275,5 +297,45 @@ public class Main {
 
 
 
+    }
+
+    public static void encounter(){
+        System.out.println("Welcome to the encounter mode");
+        System.out.println("Which mob would you like to damage?");
+        displayMobs();
+        System.out.println(" ");
+        /*for (int i=0; i<3; i++) {
+            if (mob1[i] != null) {
+                System.out.print(mob1[i] + " ");
+            }
+            if (mob2[i] != null) {
+                System.out.print(mob2[i] +" ");
+            }
+            if (mob3[i] != null) {
+                System.out.print(mob3[i]+" ");
+            }
+            if (mob4[i] != null) {
+                System.out.print(mob4[i]+" ");
+            }
+            if (mob5[i] != null) {
+                System.out.print(mob5[i]+" ");
+            }
+            if (mob6[i] != null) {
+                System.out.print(mob6[i]+" ");
+            }
+            if (mob7[i] != null) {
+                System.out.print(mob7[i]+" ");
+            }
+            if (mob8[i] != null) {
+                System.out.print(mob8[i]+" ");
+            }
+            if (mob9[i] != null) {
+                System.out.print(mob9[i]+" ");
+            }
+            if (mob10[i] != null) {
+                System.out.print(mob10[i]+" ");
+            }
+            System.out.println(" ");
+        }*/
     }
 }
